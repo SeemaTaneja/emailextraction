f55522087d27cc3e4774abfd228f67a2b3ae92dc
Seema Taneja <seemamehta001@gmail.com>
2022-04-27 12:49:07 +0100

    regular expression

--- email.js
+++ email.js
@@ -1,12 +1,8 @@
 const fs = require('fs');
 const file = fs.readFileSync("test.txt", "utf-8");
 console.log(file);
-const matches = ["@softwire.com" , "@softwire.com " , "@softwire.com\r" , "@softwire.com\n"];
-let output = 0;
-for (let i = 0; i < file.length ; i++ ){
-    if(matches.includes(file.substring(i , i+14)))
-    output += 1;  
-}
-console.log(output);
+const emailRegex = /(@softwire.com)\b/g;
+const result = file.match(emailRegex);
+console.log(result.length);
 
 
